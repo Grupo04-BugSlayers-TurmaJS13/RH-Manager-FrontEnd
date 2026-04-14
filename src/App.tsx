ed3093502605101ffc9b3bc22d1fd2170f874f78
Sabrina Novaes <sabrinanovaes96@gmail.com>
2026-04-14 18:38:25 -0300

    ✨ New Feature: Página Home

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
-import { useState } from 'react'
-
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
+import Home from './page/home/Home'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
